e56cd9648effaa0f5da0180bd028149246d05571
Juhyung990122 <harukawa99@naver.com>
2020-12-04 00:07:04 +0900

    Setting deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn mysite.wsgi --log-file -
+web: gunicorn coc_server.wsgi --log-file -
--- coc_server/settings.py
+++ coc_server/settings.py
@@ -18,9 +18,9 @@ with open(os.path.join(BASE_DIR, 'secrets.json'), 'rb') as secret_file:
 SECRET_KEY = secrets["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -72,17 +72,10 @@ WSGI_APPLICATION = 'coc_server.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangogirls',
-        'USER': 'name',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
