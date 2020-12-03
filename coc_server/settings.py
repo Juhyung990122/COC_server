84b15cb177dfc8274a74f304071811445e280193
Juhyung990122 <harukawa99@naver.com>
2020-12-03 20:45:52 +0900

    Setting heroku deplopy

--- coc_server/settings.py
+++ coc_server/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',secrets["SECRET_KEY"])
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #Debug = True
-#DEBUG = bool(os.environ.get('DJANGO_DEBUG',True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG',True))
 
 ALLOWED_HOSTS = ['*']
 
